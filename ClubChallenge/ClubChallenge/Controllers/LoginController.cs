3a44f572cfb078bad7b74c698d570ce3aeec79dc
Michael Parks <mkparks123@yahoo.com>
2020-02-25 15:57:18 -0500

    Added login and user controller

---
+++ ClubChallenge/ClubChallenge/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClubChallenge.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult AdminLogin()
+        {
+            return View();
+        }
+
+        public ActionResult UserLogin()
+        {
+            return View();
+        }
+    }
+}
