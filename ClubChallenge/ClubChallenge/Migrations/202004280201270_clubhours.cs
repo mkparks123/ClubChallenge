67eb55f1e4b15883d7087d6c7cf5cb87486bee07
Michael Parks <mkparks123@yahoo.com>
2020-04-27 22:03:08 -0400

    migrations

---
+++ ClubChallenge/ClubChallenge/Migrations/202004280201270_clubhours.cs
@@ -0,0 +1,20 @@
+﻿namespace ClubChallenge.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class clubhours : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.MemberClubHours", "ClockIn", c => c.DateTime());
+            AlterColumn("dbo.MemberClubHours", "ClockOut", c => c.DateTime());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.MemberClubHours", "ClockOut", c => c.DateTime(nullable: false));
+            AlterColumn("dbo.MemberClubHours", "ClockIn", c => c.DateTime(nullable: false));
+        }
+    }
+}
